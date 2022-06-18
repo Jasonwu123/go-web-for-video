2f3aa92f93abfd06a98f5300937183efd3057d90
Jasonwu123 <jasonwu523215@gmail.com>
2022-06-19 02:32:49 +0800

    implement dbapi to video_info

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -1,8 +1,12 @@
 package dbops
 
 import (
+	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"time"
+	"video_server/api/defs"
+	"video_server/api/utils"
 )
 
 func AddUserCredential(loginName string, pwd string) error {
@@ -10,8 +14,11 @@ func AddUserCredential(loginName string, pwd string) error {
 	if err != nil {
 		return err
 	}
-	stmtIns.Exec(loginName, pwd)
-	stmtIns.Close()
+	_, err = stmtIns.Exec(loginName, pwd)
+	if err != nil {
+		return err
+	}
+	defer stmtIns.Close()
 	return nil
 }
 
@@ -22,8 +29,11 @@ func GetUserCredential(loginName string) (string, error) {
 		return "", err
 	}
 	var pwd string
-	stmtOut.QueryRow(loginName).Scan(&pwd)
-	stmtOut.Close()
+	err = stmtOut.QueryRow(loginName).Scan(&pwd)
+	if err != nil && err != sql.ErrNoRows {
+		return "", err
+	}
+	defer stmtOut.Close()
 	return pwd, nil
 }
 
@@ -33,7 +43,88 @@ func DeleteUser(loginName string, pwd string) error {
 		log.Printf("Delete user error: %s\n", err)
 		return err
 	}
-	stmtDel.Exec(loginName, pwd)
-	stmtDel.Close()
+	_, err = stmtDel.Exec(loginName, pwd)
+	if err != nil {
+		return err
+	}
+	defer stmtDel.Close()
+	return nil
+}
+
+func AddNewVideo(aid int, name string) (*defs.VideoInfo, error) {
+	// create uuid
+	vid, err := utils.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+
+	t := time.Now()
+	ctime := t.Format("Jan 02 2006, 15:04:05")
+	stmtIns, err := dbConn.Prepare(`INSERT INTO video_info 
+	(id, author_id, name, display_ctime) VALUES (?, ?, ? ?)`)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = stmtIns.Exec(vid, aid, name, ctime)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &defs.VideoInfo{
+		Id:           vid,
+		AuthorId:     aid,
+		Name:         name,
+		DisplayCtime: ctime,
+	}
+
+	defer stmtIns.Close()
+	return res, nil
+}
+
+func GetVideoInfo(vid string) (*defs.VideoInfo, error) {
+	stmtOut, err := dbConn.Prepare("SELECT author_id, name, display_ctime FROM video_info WHERE id=?")
+	if err != nil {
+		return nil, err
+	}
+
+	var (
+		aid  int
+		dct  string
+		name string
+	)
+
+	err = stmtOut.QueryRow(vid).Scan(&aid, &name, &dct)
+	if err != nil && err != sql.ErrNoRows {
+		return nil, err
+	}
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	defer stmtOut.Close()
+
+	res := &defs.VideoInfo{
+		Id:           vid,
+		AuthorId:     aid,
+		Name:         name,
+		DisplayCtime: dct,
+	}
+	return res, nil
+}
+
+func DeleteVideoInfo(vid string) error {
+	stmtDel, err := dbConn.Prepare("DELETE FROM video_info WHERE id = ?")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmtDel.Exec(vid)
+	if err != nil {
+		return err
+	}
+
+	defer stmtDel.Close()
 	return nil
 }
--- api/defs/apidef.go
+++ api/defs/apidef.go
@@ -5,3 +5,11 @@ type UserCredential struct {
 	UserName string `json:"user_name"`
 	PWD      string `json:"pwd"`
 }
+
+// Data model
+type VideoInfo struct {
+	Id           string
+	AuthorId     int
+	Name         string
+	DisplayCtime string
+}
